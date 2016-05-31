323d666237415ff99d195cae1859bdf8407d74a6
Harun Davood <davoodharun@gmail.com>
2016-05-31 15:58:29 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+  var prompt = require('prompt');
+ 
+  // 
+  // Start the prompt 
+  // 
+  prompt.start();
+ 
+  // 
+  // Get two properties from the user: username and email 
+  // 
+  prompt.get(['client', 'email'], function (err, result) {
+  
+  });
+
